9949d8c827b6ff9bdcaede8d4e1955b99cd120e5
fzeiser <fzeiser@physnet.uni-hamburg.de>
2016-04-01 16:09:17 +0200

    changed start values

--- curve_fit.py
+++ curve_fit.py
@@ -166,9 +166,9 @@ p0=[
 	11.3, 3.2, 290, 	# (E)GLO number 1
 	14.15, 5.5, 340,  	# (E)GLO number 2
 	0.34, 				# Common (E)GLO temperature in MeV
-	1.5, 0.5, 0.68,	    # SLO number 1 (scissors 1)
-	2.5, 0.5, 0.35, 	# SLO number 2 (scissors 2)
-	7.5, 5.45, 20		# SLO number 3
+	1.9, 0.6, 0.68,	    # SLO number 1 (scissors 1)
+	2.7, 0.75, 0.35, 	# SLO number 2 (scissors 2)
+	7.5, 7, 20		# SLO number 3
 	]
 
 parameter_names = [
